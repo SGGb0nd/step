cd60f7da4487a2fd067ec8c73a272d286921fdda
SGGb0nd <lilounan1997@gmail.com>
2024-04-26 23:43:33 +0800

    feat: Add favicon to documentation for fun 😁

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ copybutton_selector = 'div.nbinput.container div.input_area div[class*=highlight
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-
+html_favicon = '_static/dalle_step_favicon.webp'
 html_theme = "furo"
 
 html_theme_options = {
